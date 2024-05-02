38ca665833288404527b0f031f4d4c4721666f46
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-05-02 06:06:49 +0530

    FC Program v4: try to create patterns

--- firstProgramme.c
+++ firstProgramme.c
@@ -1,6 +1,55 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    printf("hello, world \\n");
-   return 0;
+int main(void)
+{
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= 1; j--)
+        {
+            if (i >= j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (i >= j)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n - i + 1; j++)
+        {
+            if (i <= j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return 0;
 }
