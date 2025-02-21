c994215b460037573ec7badbc2f418184e001cff
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-02-21 23:16:45 +0530

    Commited Operator Calculator

---
+++ operators.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+    int n;
+    
+    printf("Enter first number: ");
+    scanf("%f", &num1, &num2);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    
+    printf("Enter 1 for +\n");
+    printf("Enter 2 for -\n");
+    printf("Enter 3 for *\n");
+    printf("Enter 4 for /\n");
+    scanf(" %d", &n);
+
+    switch(n) {
+        case 1:
+            printf("%.2f + %.2f = %.2f\n", num1, num2, num1 + num2);
+            break;
+        case 2:
+            printf("%.2f - %.2f = %.2f\n", num1, num2, num1 - num2);
+            break;
+        case 3:
+            printf("%.2f * %.2f = %.2f\n", num1, num2, num1 * num2);
+            break;
+        case 4:
+            if(num2 != 0)
+                printf("%.2f / %.2f = %.2f\n", num1, num2, num1 / num2);
+            else
+                printf("Error! Division by zero!\n");
+            break;
+        default:
+            printf("Error! Invalid operator!\n");
+    }
+    return 0;
+
+}
