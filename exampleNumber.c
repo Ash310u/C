874c23851ffdc28045fa24328b63db33a784bced
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-02-28 23:27:59 +0530

    Commited Maclaurin series for e^x

---
+++ exampleNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    for(i = 1; i <= n; i++) {
+        float fac = 1.0;
+        for(int j = 1; j <= i; j++) {
+            fac = fac * j;
+        }
+        sum = sum + (i/fac);
+    }
+    printf("Sum: %0.2f\n", sum);
+    return 0;
+}
