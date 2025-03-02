c90d372b11c4bfa4a8a9ec766c1a0634b8e09da0
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-03-02 14:21:14 +0530

    Implement BITWISE operator to check Odd/EVEN Number

--- oddEven.c
+++ oddEven.c
@@ -4,6 +4,6 @@ int main () {
     int n;
     printf("Give a number: ");
     scanf("%d", &n);
-    n % 2 == 0 ? printf("It's an EVEN Number.\n") : printf("It's an ODD Number.\n");
+    n & 1 == 1 ? printf("It's an ODD Number.\n") : printf("It's an EVEN Number.\n");
     return 0;
 }
