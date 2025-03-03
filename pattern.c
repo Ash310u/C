db39f56b4fed2c1451970a8211aac73e9061f0a1
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-03-03 14:23:20 +0530

    Create a Triangle pattern

--- pattern.c
+++ pattern.c
@@ -1,3 +1,19 @@
+// #include <stdio.h>
+
+// int main() {
+//     int i, j;
+//     int n;
+//     printf("Enter the number of rows: ");
+//     scanf("%d", &n);
+//     for(i = 1; i <= n; i++) {
+//         for(j = 1; j <= i; j++) {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
@@ -6,8 +22,13 @@ int main() {
     printf("Enter the number of rows: ");
     scanf("%d", &n);
     for(i = 1; i <= n; i++) {
-        for(j = 1; j <= i; j++) {
-            printf("* ");
+        for(j = 1; j <= n-i; j++) {
+            printf(" ");
+        }
+        for(j = n; j >= 1; j--) {
+            if(j <= i) {
+                printf("* ");
+            }
         }
         printf("\n");
     }
