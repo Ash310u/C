6680b783535e9857505c82f049e5df7e7ebdb466
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-03-11 20:16:49 +0530

    Commited a series

---
+++ series.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    double sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    for(i = 1; i <= n; i++) {
+        double fac = 1.0;
+        for(int j = 1; j <= i; j++) {
+            fac = fac * j;
+        }
+        sum = sum + ((i * i)/fac);
+    }
+    printf("Sum: %0.2f\n", sum);
+    return 0;
+}
