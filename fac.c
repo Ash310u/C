152012612f0ffbff53e57b0845237f1c0e076295
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-05-10 16:13:17 +0530

    feat: Create factorial of numebr using recursion

--- fac.c
+++ fac.c
@@ -1,12 +1,31 @@
+// #include <stdio.h>
+
+// int main() {
+//     int n, i , fac = 1;
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+//     for(int i = 1; i <= n; i++) {
+//         fac =  fac * i;
+//     }
+//     printf("Factorial: %d\n", fac);
+//     return 0;
+// }
+
 #include <stdio.h>
 
+long fac(int n);
 int main() {
-    int n, i , fac = 1;
+    int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++) {
-        fac =  fac * i;
-    }
-    printf("Factorial: %d\n", fac);
+    printf("Factorial: %d\n", fac(n));
     return 0;
 }
+
+long fac(int n) {
+        if(n <= 0) {
+            return 1;
+        } else {
+            return n * fac(n-1);
+        }
+    }
