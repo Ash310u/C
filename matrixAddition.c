103666cdc0cf17ce3af12e4668588ecee568744e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-30 22:00:36 +0530

    matrix Addition

--- matrixAddition.c
+++ matrixAddition.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-void inputArray(int arr[], int n) {
+void inputMatrix(int mat[][100], int n) {
     for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+        }
     }
 }
 
-void printArray(int arr[], int n) {
+void printMatrix(int mat[][100], int n) {
     for(int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        for(int j = 0; j < n; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
-void addArrays(int arr1[], int arr2[], int result[], int n) {
+void addMatrices(int mat1[][100], int mat2[][100], int result[][100], int n) {
     for(int i = 0; i < n; i++) {
-        result[i] = arr1[i] + arr2[i];
+        for(int j = 0; j < n; j++) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
     }
 }
 
 int main() {
     int n;
-    printf("Enter the size of the arrays: ");
+    printf("Enter the size of the matrices (n x n): ");
     scanf("%d", &n);
-    int arr1[n], arr2[n], result[n];
+    int mat1[100][100], mat2[100][100], result[100][100];
 
-    printf("Enter %d elements of the first array:\n", n);
-    inputArray(arr1, n);
+    printf("Elements for the first %dx%d matrix:\n", n, n);
+    inputMatrix(mat1, n);
 
-    printf("Enter %d elements of the second array:\n", n);
-    inputArray(arr2, n);
+    printf("Elements for the second %dx%d matrix:\n", n, n);
+    inputMatrix(mat2, n);
 
-    addArrays(arr1, arr2, result, n);
+    addMatrices(mat1, mat2, result, n);
 
-    printf("Resultant array after addition:\n");
-    printArray(result, n);
+    printf("Resultant matrix:\n");
+    printMatrix(result, n);
 
     return 0;
 }
